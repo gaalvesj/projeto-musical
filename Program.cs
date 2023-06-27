466cec48c9f5308adf308f46e59952eb809a2271
Gabriel <gabrielalves3654@gmail.com>
2023-06-27 13:17:42 -0300

    registro de bandas implementado

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿string firstMenssager = "Seja bem vindo ao screenSound";
 
-void ExceptionMessenger ()
+void Logo ()
 {
     // @ signifca verbatim literal
     Console.WriteLine(@"
@@ -13,10 +13,11 @@ void ExceptionMessenger ()
     Console.WriteLine(firstMenssager);
 
 }
-ExceptionMessenger();
+Logo();
 
 void ExceptionOptionsMenu () {
-
+    Logo();
+    
     Console.WriteLine("\nDigite 1 para registrar uma banda");
     Console.WriteLine("Digite 2 para mostrar todas as bandas");
     Console.WriteLine("Digite 3 para avaliar uma banda");
@@ -27,9 +28,39 @@ void ExceptionOptionsMenu () {
     string choice = Console.ReadLine()!;
 
     int choiceNumber = int.Parse(choice)!;
-    if (choiceNumber == 1) {
-    Console.WriteLine("voce digitou a opção: " + choice);
+    // if (choiceNumber == 1) {
+    // Console.WriteLine("voce digitou a opção: " + choice);
+    // } else if (choiceNumber == 2) {
+    // Console.WriteLine("voce digitou a opção: " + choice);
+    // }
+
+    switch (choiceNumber) {
+        case 1: RegisterBand ();
+            break;
+        case 2: Console.WriteLine("voce digitou a opção: " + choiceNumber);
+            break;
+        case 3: Console.WriteLine("voce digitou a opção: " + choiceNumber);
+            break;
+        case 4: Console.WriteLine("voce digitou a opção: " + choiceNumber);
+            break;
+        case 0: Console.WriteLine("tchau tchau");
+            break;
+        default: Console.WriteLine("opção invalida");
+            break;
     }
+
 }
 
+void RegisterBand () {
+    Console.Clear();
+    Console.WriteLine("Registro de bandas");
+    Console.WriteLine("digite a banda que voce deseja registar: ");
+    string band = Console.ReadLine()!;
+    Console.WriteLine($"A banda {band} foi registrada com sucesso");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExceptionOptionsMenu();
+};
+
 ExceptionOptionsMenu ();
+RegisterBand ();
