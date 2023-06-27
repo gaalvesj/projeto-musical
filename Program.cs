4383b579abd27dfbf5bb735d96e46aeb4a5821bf
Gabriel <gabrielalves3654@gmail.com>
2023-06-27 12:42:13 -0300

    primeiro tratamento de dados feito

--- Program.cs
+++ Program.cs
@@ -16,11 +16,20 @@ void ExceptionMessenger ()
 ExceptionMessenger();
 
 void ExceptionOptionsMenu () {
+
     Console.WriteLine("\nDigite 1 para registrar uma banda");
     Console.WriteLine("Digite 2 para mostrar todas as bandas");
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite 4 para mostrar a media de uma banda");
     Console.WriteLine("Digite 0 para sair");
+    
+    Console.WriteLine("\nDigite a sua opção");
+    string choice = Console.ReadLine()!;
+
+    int choiceNumber = int.Parse(choice)!;
+    if (choiceNumber == 1) {
+    Console.WriteLine("voce digitou a opção: " + choice);
+    }
 }
 
 ExceptionOptionsMenu ();
